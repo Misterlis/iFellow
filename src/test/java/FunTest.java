83dc793a2a58ff91485d34ce68fb8e6adef35fde
Misterlis <ramses.and1@gmail.com>
2022-02-17 20:37:55 +0300

    1

---
+++ src/test/java/FunTest.java
@@ -0,0 +1,29 @@
+import org.apache.logging.log4j.core.util.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class FunTest {
+    @BeforeEach
+    public void beforeAll() {
+        System.out.println("BeforeAll");
+    }
+    @AfterEach
+    public void afterAll() {
+        System.out.println("AfterAll");
+    }
+    @Test
+    public void Test1() {
+        System.out.println("1");
+        assertEquals("10","10");
+    }
+    @Test
+    public void Test2() {
+        System.out.println("2");
+        assertEquals("10", "11");
+    }
+}
