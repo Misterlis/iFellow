8f6282ea4f6d3cb0a9e04d2e95ca76368b6b3f4f
{Markov} <{ramses.and1@gmail.com}>
2022-02-20 03:43:59 +0300

    write

--- src/test/java/FunTest.java
+++ src/test/java/FunTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class FunTest {
@@ -12,10 +12,18 @@ public class FunTest {
     public void beforeAll() {
         System.out.println("BeforeAll");
     }
+
     @AfterEach
     public void afterAll() {
         System.out.println("AfterAll");
     }
+
+    @Test
+    public void main() { open("https://demoqa.com/automation-practice-form");
+    }
+
+
+
     @Test
     public void Test1() {
         System.out.println("1");
